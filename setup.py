c13422428438997eff93cb881ff8fdf80ddb25fd
Simon Dräger <sfdraeger@gmail.com>
2025-04-14 18:00:03 -0700

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="dda-py",
-    version="0.1.2",
+    version="0.1.3",
     author="Simon Draeger",
     author_email="sdraeger@salk.edu",
     description="A Python wrapper for the DDA software",
